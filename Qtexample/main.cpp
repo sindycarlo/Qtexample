f8711cd672cdf60149db92c030697fc57ed3e96c
sindycarlo <sindycarlo@gmail.com>
2016-11-24 10:23:14 +0100

    prova connect

--- Qtexample/main.cpp
+++ Qtexample/main.cpp
@@ -3,15 +3,22 @@
 #include <QFont>
 #include <QWidget>
 #include <QPushButton>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QWidget Window;
+    QVBoxLayout* layout= new QVBoxLayout;
     Window.setWindowTitle("I'm a fucking widget");
     Window.resize(200,120);
-    QPushButton quit("QUIT",&Window);
-    QObject::connect(&quit,SIGNAL(clicked()),&a,SLOT(quit()));
+    QPushButton*primo=new QPushButton("prova1",&Window);
+     QPushButton* secondo=new QPushButton("prova2",&Window);
+     secondo->setGeometry(10,40,180,40);
+     layout->addWidget(primo);
+     layout->addWidget(secondo);
+    QObject::connect(primo,SIGNAL(clicked()),&a,SLOT(quit()));
+     QObject::connect(secondo,SIGNAL(clicked()),&a,SLOT(quit()));
     Window.show();
 
     return a.exec();
