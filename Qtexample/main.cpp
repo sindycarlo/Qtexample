904236f1745b32066ccc06054dee37db9b96a280
sindycarlo <sindycarlo@gmail.com>
2016-11-24 09:47:08 +0100

    test primo esercizio

--- Qtexample/main.cpp
+++ Qtexample/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QLabel>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QPushButton hello("hello finocchio!");
+    hello.show();
 
     return a.exec();
 }
