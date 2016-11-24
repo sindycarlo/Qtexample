a5aa2699f0f4db754eed2ec7bbbb8d077a1a47cf
sindycarlo <sindycarlo@gmail.com>
2016-11-24 10:00:45 +0100

    primo esempio di SIGNAL E SLOT

--- Qtexample/main.cpp
+++ Qtexample/main.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QLabel>
+#include <QFont>
+#include <QWidget>
 #include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QPushButton hello("hello finocchio!");
-    hello.show();
+    QWidget Window;
+    Window.setWindowTitle("I'm a fucking widget");
+    Window.resize(200,120);
+    QPushButton quit("QUIT",&Window);
+    QObject::connect(&quit,SIGNAL(clicked()),&a,SLOT(quit()));
+    Window.show();
 
     return a.exec();
 }
